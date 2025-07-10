2c3112b1707698f7c6acee137d8b67bda9fb895f
AceCJM <cody@millerfamily.ca>
2025-07-09 19:20:32 -0600

    Rewrote entire project with classes updated README.md changed db formating

--- README.md
+++ README.md
@@ -4,3 +4,30 @@ a poorly made python db using plain text
 Functional if you know what your doing
 
 Documantation is a work in progress
+
+- The first line is the database name.
+- Each group is defined by a line in the format `(groupname)`.
+- Each object in a group is a line: `"obj_name",type,immutable,"value"`
+
+## Example
+
+See the [`example`](./example) file for a sample database.
+
+## Usage
+
+```python
+from plaindb import PlainDB
+
+# Load or create a database
+db = PlainDB("example")
+
+# Insert a new object
+db.insert("users", "user4", "str", False, "Diana")
+
+# Get an object's value
+value = db.get("users", "user1")
+print(value)  # Output: Alice
+
+# Delete an object
+db.delete("users", "user2")
+```
--- plaindb.py
+++ plaindb.py
@@ -1,162 +1,100 @@
+# PlainDB: A Simple Plain Text Database
 import sys,os
 
-# Global Variables:
-templateDB = "\ntempGroup\nTemp,bool,false,true" # Default DB contents
-args = ["-c","-a","-r","-l","-ddb","-e","-dt","-dd"]
+# DB template
+"""
+db name
 
+(group name)
+"obj name", "type", immutable, "value"
+"obj name", "type", immutable, "value"
+"obj name", "type", immutable, "value"
+"obj name", "type", immutable, "value"
+"""
 
-
-############################################################
-#               DB FILE MANAGEMENT FUNCTIONS              #
-############################################################
-def deleteDB(fname):
-    os.remove(f"{fname}.txt")
-
-def createDB(dbName): # Creaates default DB & writes to file
-    db = [dbName,templateDB]
-    dbStr = "".join(db)
-    with open(f"{dbName}.txt", "w") as FILE:
-        FILE.write(dbStr)
-
-def writeFile(fname,data): # Writes DB to file
-    dbList = []
-    for i in data: # Simplifies DB to lists
-        dbList.append(i)
-        if isinstance(data[i],str):
-            dbList.append(data[i])
-        elif isinstance(data[i],dict):
-            for j in data[i]:
-                dbList.append([j]+data[i][j])
-    del dbList[0]
-
-    dbSimpList = []
-    for i in dbList: # Simplifies DB to one list
-        if isinstance(i,list):
-            dbSimpList.append(",".join(i))
-        else:
-            dbSimpList.append(i)
-    dbStr = "\n".join(dbSimpList)
-
-    try:
-        with open(f"{fname}.txt", "w") as FILE: # Writes simplified DB to file
-            FILE.write(dbStr)
-    except:
-        print("ERROR while writting to file\ndb may be corupt\nExiting")
-        sys.exit()
-
-def readFile(fname): # Reads data from File
-    try:
-        with open(f"{fname}.txt", "r") as FILE:
-            cont = FILE.read()
-        return cont
-    except:
-        print("ERROR while reading file\ndb MAY be corupt\nExiting")
-        sys.exit()
-
-def loadDB(fname): # Splits DB string & Iterates contents to a dictionary
-    dbStr = readFile(fname)
-    db = {"name": fname}
-    group = {}
-    names = []
-    j = 0
-    for i in dbStr.splitlines():
-        j += 1
-        if "," in i: # Deals with Entry information
-            contents = i.split(",")
-            name = contents[0]
-            del contents[0]
-            group[name] = contents
-
-        elif j > 1: # Deals with Entry names
-            names.append(i)
-            db[names[len(names)-2]] = group
-            group = {}
-
-    db[names[len(names)-1]] = group
-    return db
-
-def printDB(fname): # Iterates DB and prints contents
-    db = loadDB(fname)
-    for i in db:
-        if isinstance(db[i],dict):
-            print("\n ",i)
-            for j,k in db[i].items():
-                print(j,":",k)
-        else:
-            print(i)
-
-
-############################################################
-#                DB DATA MANAGMENT FUNCTIONS               #
-############################################################
-
-def appendDB(db,group,name,type,imm,data): # Appends/replaces data in DB
-    data = [type,imm,data]
-    print(data)    
-    try:
-        db[group][name] = data
-    except:
-        db[group] = {name:data}
-    writeFile(db["name"],db)
+class PlainDB:
+    """A simple plain text database class for managing key-value pairs in groups."""
+    def __init__(self, db_path: str):
+        """Initializes the PlainDB instance with the path to the database file.
+        Args:
+            db_path (str): The path to the database file.
+        """
+        self.db_path = db_path
+        self.load_db()
     
-    return db
-
-def readData(db,group,name):
-    return db[group][name]
-
-def changeValue(db,group,name,data):
-    if db[group][name][1] == "true":
-        return "immutable object"
-    db[group][name][2] = data
-    writeFile(db["name"],db)
-
-def dataType(db,group,name):
-    dtype = db[group][name][0]
-    return dtype
-
-def deleteData(db,group,name):
-    if db[group][name][2] != "true":
-        del db[group][name]
-        writeFile(db["name"],db)
-        return "success"
-    else:
-        return "immutable object"
-
-def deleteGroup(db,group):
-    del db[group]
-    writeFile(db["name"],db)
-    return "success"
-
-
-
-############################################################
-#                       CLI INTERFASE                      #
-############################################################
-
-if __name__ == "__main__":
-    cli_args = sys.argv[1:]
-    if cli_args[0] in args:
-        print("MISSING DB NAME")
-        exit
-    dbName = cli_args[0]
-    for i in cli_args:
-        if i not in ["-c", "-ddb","-l"] and cli_args.index(i) == 1:
-            db = loadDB(dbName)
-            group = input("Group Name > ")
-            name = input("Entry Name > ")
-        if i == "-c":
-            createDB(dbName)
-        if i == "-l":
-            printDB(dbName)
-        if i == "-ddb":
-            print(deleteDB(dbName))
-        if i == "-a":
-            data = input("Input Data > ")
-            print(appendDB(db,group,data))
-        if i == "-r":
-            print(readData(db,group,name))
-        if i == "-e":
-            data = input("Input Data > ")
-            print(changeValue(db,group,name,data))
-        if i == "-dt":
-            print(dataType(db,group,name))
+    def load_db(self):
+        """Initializes the PlainDB instance and loads the database from the specified path."""
+        try:
+            with open(self.db_path, "r") as file:
+                cont = file.read()
+        except FileNotFoundError:
+            print(f"Database file '{self.db_path}' not found. Creating a new one.")
+            self.create_db()
+            return
+        db = cont.splitlines()
+        self.db = {"name": db[0]}
+        group = {}
+        for i in db[1:]:
+            if i.strip() == "": continue
+            if i.startswith("(") and i.endswith(")"):
+                group_name = i[1:-1]
+                group = {group_name: {}}
+                self.db.update(group)
+            else:
+                obj_name, obj_type, imm, value = i.split(",")
+                self.db[group_name][obj_name] = [obj_type, imm, value]
+    def create_db(self):
+        """Creates a new database with the specified path."""
+        self.db = {"name": self.db_path.split('/')[0]}
+        self.save_db()
+    def save_db(self):
+        """Saves the current state of the database to the specified path."""
+        with open(self.db_path, "w") as file:
+            file.write(self.db["name"] + "\n")
+            for group_name, objects in self.db.items():
+                if group_name == "name":
+                    continue
+                file.write(f"({group_name})\n")
+                for obj_name, obj_data in objects.items():
+                    file.write(f"{obj_name},{obj_data[0]},{obj_data[1]},{obj_data[2]}\n")
+    def insert(self, group_name: str, obj_name: str, obj_type: str, immutable: bool, value: str):
+        """Inserts a new object into the specified group.
+        Args:
+            group_name (str): The name of the group.
+            obj_name (str): The name of the object.
+            obj_type (str): The type of the object.
+            immutable (bool): Whether the object is immutable.
+            value (str): The value of the object.
+        """
+        if group_name not in self.db:
+            self.db[group_name] = {}
+        if obj_name in self.db[group_name]:
+            if self.db[group_name][obj_name][1] == "true":
+                print(f"Cannot modify immutable object '{obj_name}' in group '{group_name}'.")
+                return
+        self.db[group_name][obj_name] = [obj_type, str(immutable).lower(), value]
+        self.save_db()
+    def get(self, group_name: str, obj_name: str):
+        """Retrieves the value of an object from the specified group.
+        Args:
+            group_name (str): The name of the group.
+            obj_name (str): The name of the object.
+        Returns:
+            str: The value of the object, or None if not found.
+        """
+        return self.db.get(group_name, {}).get(obj_name, [None, None, None])[2]
+    def delete(self, group_name: str, obj_name: str):
+        """Deletes an object from the specified group.
+        Args:
+            group_name (str): The name of the group.
+            obj_name (str): The name of the object.
+        """
+        if group_name in self.db and obj_name in self.db[group_name]:
+            if self.db[group_name][obj_name][1] == "true":
+                raise ValueError(f"Cannot delete immutable object '{obj_name}' in group '{group_name}'.")
+            del self.db[group_name][obj_name]
+            self.save_db()
+        else:
+            print(f"Object '{obj_name}' not found in group '{group_name}'.")
+    def __repr__(self):
+        return str(self.db)
